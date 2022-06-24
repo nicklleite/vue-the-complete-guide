79536d5b4601918481cce17656e993518edcb6fb
Nicholas Leite <nicklleite@gmail.com>
2022-06-24 00:37:42 -0300

    24. Using the Native Event Object

--- 03-event-biding/app.js
+++ 03-event-biding/app.js
@@ -1,11 +1,16 @@
 const app = Vue.createApp({
     data() {
         return {
-            counter: 0
+            counter: 0,
+            name: ''
         };
     },
 
     methods: {
+        setName(e) {
+            this.name = e.target.value
+        },
+
         addToCounter(num) {
             this.counter = this.counter + num
         },
