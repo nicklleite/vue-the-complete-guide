392e100db3154305269795d86bf5cc8dfb08aee6
Nicholas Leite <nicklleite@gmail.com>
2022-07-08 23:25:35 -0300

    module 06

---
+++ 06-watchers/app.js
@@ -0,0 +1,57 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+            name: '',
+            lastName: '',
+            // fullname: '',
+        };
+    },
+    watch: {
+        counter(value) {
+            if (value > 50) {
+                this.counter = 0
+            }
+        }
+        // name(value) {
+        //     if (value === '') {
+        //         this.fullname = ''
+        //     } else {
+        //         this.fullname = value + ' ' + this.lastName
+        //     }
+        // },
+        // lastName(value) {
+        //     if (value === '') {
+        //         this.fullname = ''
+        //     } else {
+        //         this.fullname = this.name + ' ' + value
+        //     }
+        // }
+    },
+    computed: {
+        fullname() {
+            if (this.name === '' || this.lastName === '') {
+                return ''
+            }
+
+            return this.name + ' ' + this.lastName
+        }
+    },
+    methods: {
+        setName(event) {
+            this.name = event.target.value
+        },
+        add(num) {
+            this.counter = this.counter + num;
+        },
+        reduce(num) {
+            this.counter = this.counter - num;
+        },
+        resetInput() {
+            this.name = ''
+            this.lastName = ''
+        }
+    }
+});
+
+app.mount('#events');
